62859d77e3829582108e6f65228b87790e7a0085
Keldieva <56282112+Keldieva@users.noreply.github.com>
2022-11-08 22:51:38 -0500

    added my first test

--- utils/base_test.py
+++ utils/base_test.py
@@ -1,1 +1,23 @@
-from
+import unittest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+
+class BaseTest(unittest.TestCase):
+    def setUp(self, url=None):
+        service = Service(ChromeDriverManager().install())
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--window-size=1920,1080')
+        self.driver = webdriver.Chrome(service=service, options=chrome_options)
+        self.driver.get('http://babilon-m.tj')
+
+
+
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
